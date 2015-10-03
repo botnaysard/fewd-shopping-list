4e3bc7f87024948689a635d399cf69bcc79d09a3
mrmooby <scotthayward@gmail.com>
2015-10-02 21:27:03 -0400

    ready for submission

--- javascript/shopping-list.js
+++ javascript/shopping-list.js
@@ -1,26 +1,16 @@
 
 $(document).ready(function() {
-
-
-
-$('input').keydown(function(enter) { // listen for keypress
-  if(enter.keyCode == 13) { 
-    var userinput = $('input').val(); // capture content of input to variable "userinput"
-    $('.bigbox').append('<div><div class="list-item">' + userinput + '</div>' + '<div class="delete-me">x</div></div>'); // append input to bottom of list
-    $('input').val(''); // reset input
-  }
-});
-
-
-
-$("div").on('click', ".list-item", function(){
-  $(this).css('text-decoration','line-through'); // change the list item when clicked  
-});
-
-
-$("div").on('click', ".delete-me", function(){
-  $(this).prev('div.list-item').remove(); //remove the selected item
-  $(this).remove(); //remove that item's delete button
-});
-
+	$('input').keydown(function(enter) { 
+  		if(enter.keyCode == 13) { 
+    		var userinput = $('input').val().toLowerCase(); 
+    		$('.bigbox').append('<div class="container"><div class="list-item">' + userinput + '</div>' + '<div class="delete-me">x</div></div>'); 
+    		$('input').val('');
+  		}
+	});
+	$("div").on('click', ".list-item", function(){
+  		$(this).toggleClass('struck-item'); 
+	});
+	$("div").on('click', ".delete-me", function(){
+  		$(this).closest('div.container').remove(); 
+	});
 });
